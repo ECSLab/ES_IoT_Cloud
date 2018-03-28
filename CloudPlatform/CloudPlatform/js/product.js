22b180f9a2ca6d451de7e28e0dd6aed582b8e05c
zll <467171232@qq.com>
2018-03-28 12:36:15 +0800

    echart

--- CloudPlatform/CloudPlatform/js/product.js
+++ CloudPlatform/CloudPlatform/js/product.js
@@ -9,28 +9,98 @@ $(document).ready(function() {
 	
 	// var analysis = echarts.init($("#dTAnalysis"))
 	// 基于准备好的dom，初始化echarts实例
-	var myChart = echarts.init(document.getElementById('dTAnalysis'));
+	var Chart1 = echarts.init(document.getElementById('dTAnalysis'));
+	var Chart2 = echarts.init(document.getElementById('sparAnalysis'));
 
 	// 指定图表的配置项和数据
-	var option = {
-		title: {
-			text: 'ECharts 入门示例'
+	var option1 = {
+		tooltip: {
+			trigger: 'axis'
 		},
-		tooltip: {},
 		legend: {
-			data:['销量']
+			data:['设备数量']
+		},
+		grid: {
+			left: '3%',
+			right: '4%',
+			bottom: '3%',
+			containLabel: true
+		},
+		toolbox: {
+			feature: {
+				saveAsImage: {}
+			}
 		},
 		xAxis: {
-			data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+			type: 'category',
+			boundaryGap: false,
+			data: ['周一','周二','周三','周四','周五','周六','周日']
 		},
-		yAxis: {},
-		series: [{
-			name: '销量',
-			type: 'bar',
-			data: [5, 20, 36, 10, 10, 20]
-		}]
+		yAxis: {
+			type: 'value'
+		},
+		series: [
+			{
+				itemStyle : {  
+					normal : {  
+						color:'#30e5a5',  
+						lineStyle:{  
+							color:'#30e5a5'  
+						}  
+					}
+				},
+				name:'设备数量',
+				type:'line',
+				stack: '总量',
+				data:[820, 932, 901, 934, 1290, 1330, 1320]
+			}
+		]
+	};
+	var option2 = {
+		tooltip: {
+			trigger: 'axis'
+		},
+		legend: {
+			data:['触发信息数']
+		},
+		grid: {
+			left: '3%',
+			right: '4%',
+			bottom: '3%',
+			containLabel: true
+		},
+		toolbox: {
+			feature: {
+				saveAsImage: {}
+			}
+		},
+		xAxis: {
+			type: 'category',
+			boundaryGap: false,
+			data: ['周一','周二','周三','周四','周五','周六','周日']
+		},
+		yAxis: {
+			type: 'value'
+		},
+		series: [
+			{
+				itemStyle : {  
+					normal : {  
+						color:'rgb(25, 187, 255)',  
+						lineStyle:{  
+							color:'rgb(25, 187, 255)'  
+						}  
+					}
+				},
+				name:'触发信息数',
+				type:'line',
+				stack: '总量',
+				data:[820, 932, 901, 934, 1290, 1330, 1320]
+			}
+		]
 	};
 
 	// 使用刚指定的配置项和数据显示图表。
-	myChart.setOption(option);
+	Chart1.setOption(option1);
+	Chart2.setOption(option2);
 });
